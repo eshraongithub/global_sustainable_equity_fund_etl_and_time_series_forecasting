e32f82f2023ea06b990544997265bd1b7db73944
Mohamed Eshra <eshra14@hotmail.com>
2022-04-10 16:00:47 +0200

    Corrected historical data, removed duplicate function in Bollinger Bands analyses, and updated Bollinger Bands chart, added timing of Bollinger Bands analysis and Success output message

--- gsef_bollinger_bands.py
+++ gsef_bollinger_bands.py
@@ -1,9 +1,15 @@
+## Bollinger Bands
+
+import timeit
+start_time = timeit.default_timer()
+
+# Import libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-## Bollinger Bands
-
+import time
+import datetime
+import io
 
 def get_num_lines(fname):
     with open(fname) as f:
@@ -32,28 +38,16 @@ df.index = np.arange(df.shape[0])
 
 bollinger_up, bollinger_down = get_bollinger_bands(closing_prices)
 
-def get_sma(prices, rate):
-    return prices.rolling(rate).mean()
-
-def get_bollinger_bands(prices, rate= 20):
-    sma = get_sma(prices, rate)
-    std = prices.rolling(rate).std()
-    bollinger_up = sma + std * 2 # Calculate top band
-    bollinger_down = sma - std * 2 # Calculate bottom band
-    return bollinger_up, bollinger_down
-
-df.index = np.arange(df.shape[0])
-
-bollinger_up, bollinger_down = get_bollinger_bands(closing_prices)
-
 last_down_bollinger= bollinger_down.iloc[-1]
 
 last_closing_price= df.iloc[-1]['Close']
 
 if last_down_bollinger < last_closing_price:
-    to_do= 'Do not buy today'
+    to_do= 'Do not buy today.'
+elif last_up_bollinger < last_closing_price:
+    to_do= 'Sell today.'
 else:
-    to_do= 'Buy today'
+    to_do= 'Buy today.'
     
 from matplotlib.pyplot import figure
 
@@ -68,7 +62,7 @@ plt.plot(bollinger_down, label='Bollinger Down', c='m')
 plt.legend()
 
 #adding text inside the plot
-plt.text(0, 520, to_do, fontsize = 22, c='g')
+plt.text(0, 540, to_do, fontsize = 22, c='g')
 
 bollinger_bands_chart= 'gsef_folder/bollinger_bands.png'
 
@@ -81,3 +75,14 @@ else:
 plt.savefig('gsef_folder/bollinger_bands.png', dpi=100)
 
 #plt.show()
+
+# Calculate and print the time elapsed to run ETL process and the date and time of the latest run
+time_elapsed = timeit.default_timer() - start_time
+
+time_elapsed_minutes = int((time_elapsed % 3600) // 60)
+
+time_elapsed_seconds = int(time_elapsed % 60)
+
+now = datetime.datetime.now()
+
+print("\n Success! Your Bollinger Bands chart has been updated in {} minutes and {} seconds on {}".format(time_elapsed_minutes, time_elapsed_seconds, now.strftime("%Y-%m-%d %H:%M:%S")))
